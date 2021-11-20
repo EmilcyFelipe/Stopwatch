861c9770383a466d56190f6573f995ff6f0005e6
Felipe de Paula <emilcyfelipe@gmail.com>
2021-11-20 19:13:52 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+  
+let timer = null;
+let ds = 0;
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
 export default function App() {
+  const [ playText, setPlayText ] = useState('Go')
+  const [ time, setTime ] = useState('00:00:00');
+  const [ recordList, setRecordList ] = useState([]);
+  const [ runnig, setRunnig ] = useState(false);
+  
+
+  function play(){
+    if(timer === null){
+      setPlayText('Stop');
+      timer = setInterval(()=>{
+        ds++;
+        if(ds==10){
+          ds=0;
+          ss++
+        }
+        if(ss==60){
+          ss=0;
+          mm++;
+        }
+        if(mm==60){
+          mm=0;
+          hh++
+        }
+        let format = (hh>0?(hh<10?'0'+hh:hh)+':':'')+(mm<10?'0'+mm:mm)+':'+(ss<10?'0'+ss:ss)+':'+(ds<10?'0'+ds:ds);
+        setTime(format)
+      },100)
+    }else{
+      clearInterval(timer);
+      setPlayText('Go')
+      timer = null;
+    }
+  }
+
+  function record(){
+
+  }
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.display}>
+        <Text style={{fontWeight:'bold', color:'#FFF', fontSize: 48}}>{time}</Text>
+      </View>
+      <View style={styles.actions}> 
+        <TouchableOpacity onPress={play}>
+          <View style={styles.play}>
+            <Text style={{fontWeight:'bold', fontSize: 20, color: '#FFF'}}>{playText}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.mark} onPress={record}>
+          <Ionicons name='alarm' color='#FFF' size={50}/>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -14,8 +71,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#101010',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  display:{
+    width: 350,
+    height: 350,
+    borderRadius: 300/2,
+    borderWidth: 10,
+    borderColor: "#FFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100
+  },
+  actions:{
+    position: 'absolute',
+    bottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  play:{
+    width: 100,
+    height: 100,
+    backgroundColor: '#36578A',
+    borderRadius: 300/2,
+    borderWidth: 6,
+    borderColor: "#FFF",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  mark:{
+    position: 'absolute',
+    right: -80,
+  }
 });
